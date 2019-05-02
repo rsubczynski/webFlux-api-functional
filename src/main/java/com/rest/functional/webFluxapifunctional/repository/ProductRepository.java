31f46c2a3837f54a64088f1d3335bce7c119d350
Radosław Subczyński <radoslaw.subczynski@likims.com>
2019-05-02 12:46:26 +0200

    webFlux-api-functional

---
+++ src/main/java/com/rest/functional/webFluxapifunctional/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.rest.functional.webFluxapifunctional.repository;
+
+import com.rest.functional.webFluxapifunctional.model.Product;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends ReactiveMongoRepository<Product, String> {
+
+}
---
+++ src/test/java/com/rest/functional/webFluxapifunctional/WebFluxApiFunctionalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rest.functional.webFluxapifunctional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebFluxApiFunctionalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
